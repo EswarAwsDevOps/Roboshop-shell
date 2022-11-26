9f9281a7248919139da6fc9aa751004d5ddc1359
eswar <eswar.awsdevops@gmail.com>
2022-11-26 11:03:10 +0530

    user script

---
+++ user.sh
@@ -0,0 +1,26 @@
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+yum install nodejs -y
+useradd roboshop
+
+
+curl -s -L -o /tmp/user.zip "https://github.com/roboshop-devops-project/user/archive/main.zip"
+cd /home/roboshop
+rm-rf user
+unzip -o /tmp/user.zip
+mv user-main user
+cd /home/roboshop/user
+npm install
+
+
+
+#Update `REDIS_ENDPOINT` with Redis Server IP
+
+#Update `MONGO_ENDPOINT` with MongoDB Server IP
+
+
+
+# mv /home/roboshop/user/systemd.service /etc/systemd/system/user.service
+# systemctl daemon-reload
+# systemctl start user
+# systemctl enable user
+
